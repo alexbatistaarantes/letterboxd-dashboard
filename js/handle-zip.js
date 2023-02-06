e4a9f649f71421941edff3773bed3b3a2522d0c6
alexbatistaarantes <alexbatistaarantes@gmail.com>
2023-02-05 21:32:30 -0300

    Read tables to datasets

--- js/handle-zip.js
+++ js/handle-zip.js
@@ -2,29 +2,26 @@ const dataInput = document.querySelector("#data-input");
 const buttonCreateDashboard = document.querySelector("#create-dashboard-button");
 const filesList = document.querySelector("#files-list");
 
-let files = {
-    'watched': null,
-    'ratings': null,
-    'diary': null,
-    'watchlist': null
-};
+let datasets = {
+    watched: null
+}
 
 /* Create Dashboard */
 buttonCreateDashboard.addEventListener('click', async () => {
     await loadDataFiles();
-    console.log(files);
-    getMoviesWatched();
+    console.log(datasets);
 });
 
 /* Get movies watched */
-async function getMoviesWatched(){
-    const reader = new FileReader();
-
-    let textWriter = new zip.TextWriter();
-
-    //reader.readAsText(await files['watched'].getData(textWriter))
-    const content = await files['watched'].getData(textWriter);
-    console.log(content);
+async function loadTable(dataFile, key){
+    let blobWriter = new zip.BlobWriter();
+    dfd.readCSV(await dataFile.getData(blobWriter))
+    .then((df) => {
+        datasets[key] = df
+    })
+    //const reader = new FileReader();
+    //let textWriter = new zip.TextWriter();
+    //const content = await dataFile.getData(textWriter);
 }
 
 /* Get data files from zip */
@@ -32,8 +29,13 @@ async function loadDataFiles() {
     const zipFile = new zip.ZipReader(new zip.BlobReader( dataInput.files[0] ));
     const filesList = await zipFile.getEntries();
     
+    let files = {};
     files['watched'] = filesList.filter((file) => file.filename === "watched.csv")[0];
+    await loadTable(files['watched'], 'watched');
     files['ratings'] = filesList.filter((file) => file.filename === "ratings.csv")[0];
+    await loadTable(files['ratings'], 'ratings');
     files['diary'] = filesList.filter((file) => file.filename === "diary.csv")[0];
+    await loadTable(files['diary'], 'ratings');
     files['watchlist'] = filesList.filter((file) => file.filename === "watchlist.csv")[0];
+    await loadTable(files['watchlist'], 'watchlist');
 }
